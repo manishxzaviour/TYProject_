dd99cfafc8e6e11969562a831edaa8331acb74d7
Manish patil <68430718+manishxzaviour@users.noreply.github.com>
2023-01-24 15:43:24 +0530

    update

--- pythonOPENCV.py
+++ pythonOPENCV.py
@@ -3,7 +3,7 @@ import cv2 as cv
 import time
 import numpy as np
 import math
-
+#
 # st = time.time()
 # image = cv.imread("D:\my stuff\TYProject\DIP_P1\capture5.jpg")
 # image = image[0:300, 50:700];
@@ -39,32 +39,41 @@ import math
 #             cdstP[x][y]=[0,0,255]
 #             faultL.append([x,y]);
 #             probF+=1
-# # cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+# cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
 # print(time.time() - st)
 # if(probF>threshC):
 #     print('!! mechanical failure detected !!')
 #     print('fault detected at pixel locations:')
 #     print(faultL)
+# cv.waitKey(0)
 
-url ='http://192.168.0.104/capture?_cb='
-requests.get('http://192.168.0.104/control?var=framesize&val=8')
-while(True):
+
+
+
+print('Enter ip:')
+urlH = 'http://'+input()
+url = urlH+'/capture?'
+requests.get(urlH+'/control?var=framesize&val=8')
+threshP = 40
+threshC = 100
+probF = 0;
+faultL = []
+pos1 = 0
+pos2 = 0
+
+while (1):
     st = time.time()
-    img = requests.get(url + str(time.time()), stream=True).raw
+    img = requests.get(url, stream=True).raw
     image = np.asarray(bytearray(img.read()), dtype='uint8')
     image = cv.imdecode(image, cv.IMREAD_COLOR)
-    image=image.astype(np.uint8)
     image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    image = image[0:300, :];
-    imageC = cv.bitwise_not(image);
+    image = image[0:300, :]
     image = cv.filter2D(image, -1, np.ones([5, 5]) / 25)
     imageEnh = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
     canny_output = cv.Canny(imageEnh, 80, 150, )
     cannyBlb = cv.dilate(canny_output, np.ones([5, 5], np.uint8));
     cdstP = cv.cvtColor(cannyBlb, cv.COLOR_GRAY2BGR)
     linesP = cv.HoughLinesP(cannyBlb, 1, np.pi / 180, 50, None, 50, 10)
-    pos1 = 0
-    pos2 = 0
     if linesP is not None:
         for i in range(0, len(linesP)):
             l = linesP[i][0]
@@ -75,23 +84,21 @@ while(True):
     avp = pos1 + pos2
     avp /= 2
     cv.line(cdstP, (0, pos1), (650, pos2), (255, 150, 120), 1, cv.LINE_AA)
-    threshP = 40
-    threshC = 100
-    probF = 0;
-    faultL = []
     for x in range(0, 300):
         for y in range(0, 640):
             if canny_output[x][y] == 255 and abs(x - avp) > threshP:
                 cdstP[x][y] = [0, 0, 255]
                 faultL.append([x, y]);
                 probF += 1
+    cv.imwrite('img1.png',cdstP)
     cv.imshow('img', cdstP)
-    # if (probF > threshC):
-    #     print('!! mechanical failure detected !!')
-    #     print('fault detected at pixel locations:')
-    #     print(faultL)
     en = time.time()
     print((en - st) * 1000)
     if cv.waitKey(25) & 0xFF == ord('q'):
-       break
+        break
+    if (probF > threshC):
+        print('!! mechanical failure detected !!')
+        print('fault detected at pixel locations:')
+        print(faultL)
+        pass
 cv.waitKey()
